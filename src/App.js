339a944ae294b3fcb6e0c5dbcc7b5e50ca7a983a
Nicholas Huynh <nickhuynhq@gmail.com>
2022-11-15 13:56:56 -0500

    Implement Delete functionality

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Posts from "./components/Posts/Posts";
 import useStyles from "./styles";
 
 const App = () => {
-  const [currentId, setCurrentId] = useState(null);
+  const [currentId, setCurrentId] = useState(0);
   const classes = useStyles();
   const dispatch = useDispatch();
 
--- src/components/Posts/Post/Post.js
+++ src/components/Posts/Post/Post.js
@@ -10,14 +10,18 @@ import {
 import ThumbUpAltIcon from "@material-ui/icons/ThumbUpAlt";
 import DeleteIcon from "@material-ui/icons/Delete";
 import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
+import NoImage from "../../../assets/images/no-image.png";
+
 import moment from "moment";
 import { useDispatch } from "react-redux";
-import NoImage from "../../../assets/images/no-image.png"
+import { deletePost } from "../../../actions/posts";
 
 import useStyles from "./styles";
 
 const Post = ({ post, setCurrentId }) => {
   const classes = useStyles();
+  const dispatch = useDispatch();
+
   return (
     <Card className={classes.card}>
       <CardMedia
@@ -48,8 +52,8 @@ const Post = ({ post, setCurrentId }) => {
         </Typography>
       </div>
       <Typography className={classes.title} variant="h5" gutterBottom>
-          {post.title}
-        </Typography>
+        {post.title}
+      </Typography>
       <CardContent>
         <Typography className={classes.title} variant="h6" gutterBottom>
           {post.message}
@@ -61,7 +65,11 @@ const Post = ({ post, setCurrentId }) => {
           Like
           {post.likeCount}
         </Button>
-        <Button size="small" color="primary" onClick={() => {}}>
+        <Button
+          size="small"
+          color="primary"
+          onClick={() => dispatch(deletePost(post._id))}
+        >
           <DeleteIcon fontSize="small" />
           Delete
         </Button>
