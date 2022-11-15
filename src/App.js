116efe432899b12bbcf69a7e13991beb16de372d
Nicholas Huynh <nickhuynhq@gmail.com>
2022-11-14 19:52:32 -0500

    Set up JSX structure and load in assets

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
+import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+import memories from "./images/memories.png";
+import Form from "./components/Form/Form";
+import Posts from "./components/Posts/Posts";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <Container maxidth="lg">
+      <AppBar position="static" color="inherit">
+        <Typography variant="h2" align="center">
+          Memories
+        </Typography>
+        <img src={memories} alt="memories" height="60" />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
+  );
+};
 
-export default App
+export default App;
