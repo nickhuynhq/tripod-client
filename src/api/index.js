bad4f8c8fe0feccc70cb707dd5c3f4ec71c9f179
Nicholas Huynh <nickhuynhq@gmail.com>
2022-11-16 21:01:11 -0500

    Add interceptor to API requests to check for token

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,14 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5001" });
+const API = axios.create({ baseURL: `${process.env.REACT_APP_API_URL}` });
 
-const url = `${process.env.REACT_APP_API_URL}`;
+API.interceptors.request.use((req) => {
+  if (localStorage.getItem('profile')) {
+    req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem('profile')).token}`;
+  }
+
+  return req;
+});
 
 export const fetchPosts = () => API.get("/posts");
 export const createPost = (newPost) => API.post("/posts", newPost);
