ddcca40f735c098c8cbdabefc6981c27b1af2d0f
Nicholas Huynh <nickhuynhq@gmail.com>
2022-11-16 20:48:49 -0500

    Set up Sign In and Sign Up functionality with API

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -2,23 +2,25 @@ import { AUTH } from "../constants/actionTypes";
 import * as api from "../api";
 
 export const signin = (formData, navigate) => async (dispatch) => {
-    try {
-        // log in the user...
+  try {
+    const { data } = await api.signIn(formData);
 
-        navigate("/")
-    } catch (error) {
-        console.log(error)
-    }
-}
+    dispatch({ type: AUTH, data });
 
-
+    navigate("/");
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 export const signup = (formData, navigate) => async (dispatch) => {
-    try {
-        // sign up the user...
+  try {
+    const { data } = await api.signUp(formData);
 
-        navigate("/")
-    } catch (error) {
-        console.log(error)
-    }
-}
+    dispatch({ type: AUTH, data });
+
+    navigate("/");
+  } catch (error) {
+    console.log(error);
+  }
+};
