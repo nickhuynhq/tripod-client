44f2fb22620d3b0d5ceaff1db99dfa00b8dcaf5c
Nicholas Huynh <nickhuynhq@gmail.com>
2022-11-18 14:11:22 -0500

    Add and Style search to Home component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,17 +1,61 @@
 import React, { useEffect, useState } from "react";
-import { Container, Grow, Grid, Paper } from "@material-ui/core";
+import {
+  Container,
+  Grow,
+  Grid,
+  Paper,
+  AppBar,
+  TextField,
+  Button,
+} from "@material-ui/core";
 import { useDispatch } from "react-redux";
 
 import { getPosts } from "../../actions/posts";
 import Form from "../Form/Form";
 import Posts from "../Posts/Posts";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import Pagination from "../Pagination";
+import ChipInput from "material-ui-chip-input";
+import useStyles from "./styles";
+
+function useQuery() {
+  return new URLSearchParams(useLocation().search);
+}
 
 const Home = () => {
-  const [currentId, setCurrentId] = useState(0);
   const dispatch = useDispatch();
   const location = useLocation();
+  const query = useQuery();
+  const page = query.get("page");
+  const searchQuery = query.get("searchQuery");
+  const classes = useStyles();
+  const navigate = useNavigate();
+
+  const [currentId, setCurrentId] = useState(0);
+  const [search, setSearch] = useState("");
+  const [tags, setTags] = useState([]);
+
+  const handleKeyPress = (e) => {
+    if (e.keyCode === 13) {
+      searchPosts();
+    }
+  };
+
+  const handleAdd = (tag) => {
+    setTags([...tags, tag]);
+  };
+
+  const handleDelete = (tagToDelete) => {
+    setTags(tags.filter((tag) => tag !== tagToDelete));
+  };
+
+  const searchPosts = () => {
+    if (search.trim()) {
+      // dispatch to fetch search post
+    } else {
+      navigate("/");
+    }
+  };
 
   useEffect(() => {
     dispatch(getPosts());
@@ -19,17 +63,52 @@ const Home = () => {
 
   return (
     <Grow in>
-      <Container>
+      <Container maxWidth="xl">
         <Grid
+          className={classes.gridContainer}
           container
           justifyContent="space-between"
           alignItems="stretch"
           spacing={3}
         >
-          <Grid item xs={12} sm={7}>
+          <Grid item xs={12} sm={6} md={9}>
             <Posts setCurrentId={setCurrentId} />
           </Grid>
-          <Grid item xs={12} sm={4}>
+
+          <Grid item xs={12} sm={6} md={3}>
+            <AppBar
+              className={classes.appBarSearch}
+              position="static"
+              color="inherit"
+            >
+              <TextField
+                name="search"
+                variant="outlined"
+                label="Search Memories"
+                fullWidth
+                value={search}
+                onKeyPress={handleKeyPress}
+                onChange={(e) => {
+                  setSearch(e.target.value);
+                }}
+              />
+              <ChipInput
+                style={{ margin: "10px 0" }}
+                value={tags}
+                onAdd={handleAdd}
+                onDelete={handleDelete}
+                label="Search Tags"
+                variant="outlined"
+              />
+              <Button
+                onClick={searchPosts}
+                className={classes.searchButton}
+                color="primary"
+                variant="contained"
+              >
+                Search
+              </Button>
+            </AppBar>
             <Form currentId={currentId} setCurrentId={setCurrentId} />
             <Paper elevation={6}>
               <Pagination />
--- src/components/PostDetails/PostDetails.jsx
+++ src/components/PostDetails/PostDetails.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 const PostDetails = () => {
+    
   return (
     <div>PostDetails</div>
   )
