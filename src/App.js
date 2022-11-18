ec2b87489a0b3c548b15e6cb32ebb126ac60c3ff
Nicholas Huynh <nickhuynhq@gmail.com>
2022-11-17 22:54:58 -0500

    Set up additional routes and route logic, as well as PostDetails component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
 import React from "react";
 import { Container } from "@material-ui/core";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/Home/Home";
 import Auth from "./components/Auth/Auth";
+import PostDetails from "./components/PostDetails/PostDetails";
 
 const App = () => {
+
+  const user = JSON.parse(localStorage.getItem('profile'));
+
   return (
     <GoogleOAuthProvider clientId={`${process.env.REACT_APP_GOOGLE_API_TOKEN}`}>
       <BrowserRouter>
-        <Container maxidth="lg">
+        <Container maxidth="xl">
           <Navbar />
           <Routes>
-            <Route path="/" exact element={<Home />} />
-            <Route path="/auth" exact element={<Auth />} />
+            <Route path="/" exact element={<Navigate to="/posts" />} />
+            <Route path="/posts" exact element={<Home />} />
+            <Route path="/posts/search" exact element={<Home />} />
+            <Route path="/posts/:id" element={<PostDetails />} />
+            <Route path="/auth" exact element={!user ? <Auth /> : <Navigate to="/posts"/>} />
+            <Route path="*" exact element={<h1>404</h1>} />
           </Routes>
         </Container>
       </BrowserRouter>
